5732ea4db4de656e3fb240dea0e6b1f43c7d6917
xpple <info@ausgames.net>
2020-12-16 18:14:47 +0100

    Try different ping command x9

--- utils.php
+++ utils.php
@@ -13,15 +13,12 @@ function getIp() {
 
 function getPing() {
 	$ip = getIp();
-	/*if (strpos($ip, ":") === false) {
+	if (strpos($ip, ":") === false) {
         exec("ping -c 1 " . $ip . " | head -n 2 | tail -n 1 | awk '{print $7}'", $ping);
     }
 	else {
         exec("ping -6 -c 1 " . $ip . " | head -n 2 | tail -n 1 | awk '{print $7}'", $ping);
-    }*/
-	exec("ping -n 3 $ip", $ping);
-
-	/*return explode("=", $ping[0])[1];*/
+    }
     echo json_encode($ping);
     return $ping;
 }
