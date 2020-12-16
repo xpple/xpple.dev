04d3a0d7c37c4a3b1a97b71afb92682bc0bdf6f4
xpple <info@ausgames.net>
2020-12-16 18:08:45 +0100

    Try different ping command x4

--- utils.php
+++ utils.php
@@ -22,7 +22,7 @@ function getPing() {
 	exec("ping -n 3 $ip", $ping);
 
 	/*return explode("=", $ping[0])[1];*/
-    return explode("=", $ping)[1];
+    return $ping;
 }
 
 switch($_SERVER['QUERY_STRING']) {
