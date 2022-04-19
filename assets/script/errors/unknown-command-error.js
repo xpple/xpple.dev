e689f975953bac34770d85286a1b45200362842f
Fred <frederik.v.d.els@outlook.com>
2022-04-19 20:28:09 +0200

    Change unknown command error message

--- assets/script/errors/unknown-command-error.js
+++ assets/script/errors/unknown-command-error.js
@@ -3,7 +3,7 @@ import {CommandSyntaxError} from "./command-syntax-error.js";
 export class UnknownCommandError extends CommandSyntaxError {
 
     constructor(commandString) {
-        super(`Unknown command: ${commandString}`);
+        super(`Unknown command: ${commandString}. Execute 'help' for a list of commands.`);
     }
 
     getName() {
