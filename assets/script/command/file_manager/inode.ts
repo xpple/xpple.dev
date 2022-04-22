4fda6e783780228db00d086dd0fba06acde98e85
networkException <git@nwex.de>
2022-04-23 01:26:46 +0200

    INode: Add helper to calculate path
    This patch adds a new helper to inode returning its absolute path

--- assets/script/command/file_manager/inode.ts
+++ assets/script/command/file_manager/inode.ts
@@ -3,4 +3,22 @@ import { Directory } from "./directory";
 export abstract class INode {
     public constructor(public name: string, public parent?: Directory) {
     }
+
+    public getPath(): string {
+        const parts = [];
+
+        let directory: Directory | undefined;
+
+        do {
+            let currentINode: INode = directory ?? this;
+
+            parts.push(currentINode.name);
+
+            directory = currentINode.parent;
+        } while (directory !== undefined)
+
+        parts.reverse();
+
+        return parts.join('/');
+    }
 }
